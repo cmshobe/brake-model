01b577833e085d08e93194418d7674098cd4dce0
Mark Piper <mark.piper@colorado.edu>
2017-06-03 15:14:39 -0600

    Inlcude basic_modeling_interface package in requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='BRaKE',
       author_email='charles.shobe@colorado.edu',
       license='GNU GPLv3',
       description='Blocky River and Knickpoint Evolution Model',
-      install_requires=('ruamel.yaml', 'scipy', 'numpy',),
+      install_requires=('ruamel.yaml', 'scipy', 'numpy',
+                        'basic_modeling_interface',),
       packages=find_packages(exclude=['*.tests']),
 )
